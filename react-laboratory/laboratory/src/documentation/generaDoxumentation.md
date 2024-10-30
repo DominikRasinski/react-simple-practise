9d6a22edc0327fcd6b30f4fc23d7a5684ed24e6e
Dominik Rasiński <dox.gmr@gmail.com>
2024-10-30 15:51:52 +0100

    Add basic documentation and enhance UseContextLab component

---
+++ react-laboratory/laboratory/src/documentation/generaDoxumentation.md
@@ -0,0 +1,7 @@
+# Podstawowa dokumentacja ogólnikowa
+
+Zawarte informacje...
+
+## Słownik
+
+- Subskrybent - w programowaniu oznacza to zachowanie obiektu/mechanizmu który obserwuje klasę, obiekt, funkcję zwała jak zwał element programu który w przypadku spełnienia opisanych w nim reguł wyemituje informację, która obserwator/subskrybent będzie mógł przechwycić i wykorzystać w swojej implementacji.
--- react-laboratory/laboratory/src/labolatory/useContextLab.tsx
+++ react-laboratory/laboratory/src/labolatory/useContextLab.tsx
@@ -1,9 +1,21 @@
-import { LaboContainer } from '../components/laboContainer';
+import { LaboContainer } from "../components/laboContainer";
+import { InsideContainers } from "../components/InsideContainers";
+import { HighlighSyntax } from "../components/HighlighSyntax";
+
+const codeExample = `
+  const value = useContext(SomeContext)
+`;
 
 export const UseContextLab = () => {
   return (
     <LaboContainer>
-      <div>UseContextLab</div>
+      <InsideContainers type="toLeft">
+        <div>
+          UseContext jest specjalnym hookiem który pozwala na
+          subskrypcję/obserwację context'u znajdującego się w komponencie
+        </div>
+        <HighlighSyntax codeString={codeExample} />
+      </InsideContainers>
     </LaboContainer>
   );
 };
