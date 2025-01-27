297b2605c4356234abd617776672367f26394c1a
Dominik Rasiński <dox.gmr@gmail.com>
2025-01-27 01:53:13 +0100

    feat: add environment configuration and update supabase integration

--- nextjs-laboratory/next-laboratory/src/services/supabase.ts
+++ nextjs-laboratory/next-laboratory/src/services/supabase.ts
@@ -1,5 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import '@/settings/envConfig'
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://snpoauzfudpkvqxrksnx.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = 'https://snpoauzfudpkvqxrksnx.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
