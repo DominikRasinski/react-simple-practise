f3fe93a6e4bd11b4120c9e80b8aa495995b61a03
Dominik Rasiński <dox.gmr@gmail.com>
2025-01-27 20:52:24 +0100

    feat: implement cabin fetching and initialize supabase client

--- nextjs-laboratory/next-laboratory/src/services/supabase.ts
+++ nextjs-laboratory/next-laboratory/src/services/supabase.ts
@@ -1,6 +1,14 @@
-import '@/settings/envConfig'
-import { createClient } from '@supabase/supabase-js'
+import "@/settings/envConfig";
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://snpoauzfudpkvqxrksnx.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = "https://snpoauzfudpkvqxrksnx.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY!;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+if (!supabase) {
+  throw new Error("supabase is not initialized");
+}
+
+console.log("supabase is initialized");
+
+export default supabase;
